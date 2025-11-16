c6de40ecc89888b61c8386883e7e3ea44fb72aad
aznoH333 <glentik23@gmail.com>
2025-11-16 14:32:44 +0100

    action modals

--- frontend/src/modules/listOverview/listCard/ListCard.tsx
+++ frontend/src/modules/listOverview/listCard/ListCard.tsx
@@ -5,6 +5,8 @@ import {User} from "@/src/types/User";
 import {getUserRightsForAList} from "@/src/types/ShoppingListUser";
 import {Button} from "@/src/modules/input/button/Button";
 import {useRouter} from "next/navigation";
+import {useState} from "react";
+import {Modal} from "@/src/modules/modal/Modal";
 
 interface ListCardProps {
     list: ShoppingList,
@@ -18,6 +20,9 @@ export function ListCard({list, loggedInUser, onListDelete, onListArchive}: List
     const userRights = getUserRightsForAList(listUser);
 
     const router = useRouter();
+
+    const [archiveModalOpen, setArchiveModalOpen] = useState(false);
+    const [deleteModalOpen, setDeleteModalOpen] = useState(false);
 
 
     const completedCount = list.items.filter((it)=>it.state == "checked").length
@@ -37,17 +42,15 @@ export function ListCard({list, loggedInUser, onListDelete, onListArchive}: List
             <div className={styles.buttons}>
                 {userRights.canArchiveList && list.state !== "archived" && (
                     <Button onClick={() => {
-                        onListArchive(list.id)
+                        setArchiveModalOpen(true);
                     }}>Archive</Button>
                 )}
 
                 {userRights.canDeleteList && list.state !== "archived"  && (
                     <Button onClick={() => {
-                        onListDelete(list.id)
+                        setDeleteModalOpen(true)
                     }}>Delete</Button>
                 )}
-
-
 
             </div>
             <div className={styles.buttons}>
@@ -58,5 +61,23 @@ export function ListCard({list, loggedInUser, onListDelete, onListArchive}: List
 
             </div>
         </div>
+
+        <Modal isOpen={archiveModalOpen} setIsOpen={setArchiveModalOpen} onConfirm={()=>
+            {
+                onListArchive(list.id);
+                setArchiveModalOpen(false);
+            }
+        }>
+            Are you sure you want to archive {list.name}?
+        </Modal>
+
+        <Modal isOpen={deleteModalOpen} setIsOpen={setDeleteModalOpen} onConfirm={()=>
+        {
+            onListDelete(list.id);
+            setDeleteModalOpen(false);
+        }
+        }>
+            Are you sure you want to delete {list.name}?
+        </Modal>
     </div>
 }
